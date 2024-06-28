e653a2ed6d915b90625065c77bc83457145d4c97
kkkkimtaehyeon <kth1927@naver.com>
2024-06-28 21:32:55 +0900

    tts 전송 기능 구현

--- routes/websocket.py
+++ routes/websocket.py
@@ -1,19 +1,30 @@
-from fastapi import WebSocket, APIRouter
-import json
+from fastapi import WebSocket, APIRouter, WebSocketException, WebSocketDisconnect
+from openai import OpenAI
+import os
+import io
+from dotenv import load_dotenv, find_dotenv
+from DB.schemas import MenuService
+
+_= load_dotenv(find_dotenv())
+menu_service = MenuService()
+
 router = APIRouter()
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
-gpt_general_response = {
-            "isMenu": False,
-            "isOrder": False,
-            "isAnswer": True,
-            "content": {
-                "answer": "gpt의 답변 예시입니다.",
-                "menus": {}
-            }
-        }
-
-
-
+DUMMY_DATA = {
+                    "isMenu": True,
+                    "isOrder": False,
+                    "isAnswer": True,
+                    "content": {
+                        "answer": "아메리카노를 추가하셨습니다.",
+                        "menus": {
+                                "id": "667cf9d8bf0cbc39cdf4ced1",
+                                "name": "아메리카노",
+                                "price": 3000,
+                                "amount": 1,
+                            }
+                    }
+                }
 
 @router.websocket("/conversations")
 async def conversation(websocket: WebSocket):
@@ -21,38 +32,49 @@ async def conversation(websocket: WebSocket):
     try:
         while True:
             text = await websocket.receive_text()
-            print(text)
+            try:
+                # gpt 로직 추가
+                gpt_response = DUMMY_DATA
+
+                if gpt_response["isAnswer"]:
+                    answer = gpt_response["content"]["answer"]
+                    audio_buffer = await openai_tts(answer)
+                    await websocket.send_bytes(audio_buffer)
+
+                if gpt_response["isMenu"]:
+                    menu_id = gpt_response["content"]["menus"]["id"]
+
+                    await validate_menu(id=menu_id)
+
+                await websocket.send_json(gpt_response)
+                
+            except Exception as e:
+                print('에러 발생', e)
+
+    except WebSocketDisconnect:
+        print('연결 종료')
+    except WebSocketException:
+        print('웹소켓 에러')        
             
-            #gpt 로직 추가
-            gpt_response = {
-                "isMenu": True,
-                "isOrder": False,
-                "isAnswer": True,
-                "content": {
-                    "answer": "아메리카노를 추가하셨습니다.",
-                    "menus": {
-                            "id": 1,
-                            "name": "아메리카노",
-                            "price": 3000,
-                            "amount": 1,
-                        }
-                }
-            }
-            #response = validateResponse(gpt_response)
-            await websocket.send_json(gpt_response)
-            print(gpt_response)
-            
+async def validate_menu(id: str) -> bool:
+    try:
+        menu_service.fetch_menu(id)
+        print('메뉴 있음')
+        pass
     except Exception as e:
-        print(e)
-        print('음성인식 종료')
+        print('메뉴 없음', e)
 
-def validateResponse(response):
-    # 메뉴 추가
-    if response["isOrder"]:
-        return response["content"]
-    # gpt 답변
-    else:
-        return response["content"]["script"]
+
+async def openai_tts(text):
+    buffer=io.BytesIO()
+    with client.audio.speech.with_streaming_response.create(
+        model="tts-1",
+        voice="alloy",
+        input=text,
+        response_format="opus"
+    ) as stream:
+        for chunk in stream.iter_bytes(chunk_size=1600):
+            buffer.write(chunk)
     
-
-
+    buffer.seek(0)
+    return buffer.read()  
